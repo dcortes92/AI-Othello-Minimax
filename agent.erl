6f8d876a167e4d353afbb406562b269db39e7b7e
Daniel <dcortes92@hotmail.com>
2013-09-26 15:03:46 -0600

    Evaluation Function

--- agent.erl
+++ agent.erl
@@ -103,22 +103,22 @@ make_move(Player, Board, _Depth, _Border) -> 57. %Esta tupla se cambia por un n�
 %Se le envía una lista con todos los posibles movimientos de un jugador
 %la lista tiene números del 12-19,22-29, y retorna una tupla con las
 %posibles casillas en donde se puede mover.
-posibles_movimientos() ->
-	list_to_tuple(posibles_movimientos(movimientos())).
+get_moves() ->
+	list_to_tuple(get_moves(valid_moves())).
 
-posibles_movimientos([H|T]) ->
+get_moves([H|T]) ->
 	Valido = othello:check_move(H, othello:board(), othello:directions(), -1),
 	if Valido ->
-		[H |  posibles_movimientos(T)];
+		[H |  get_moves(T)];
 	true ->
-		posibles_movimientos(T)
+		get_moves(T)
 	end;
 
-posibles_movimientos([]) -> [].
+get_moves([]) -> [].
 
 
-%Genera la lista de movimientos que utiliza posibles_movimientos
-movimientos() ->
+%Genera la lista de movimientos que utiliza get_moves
+valid_moves() ->
 	lists:seq(12,19) ++
 	lists:seq(22,29) ++
 	lists:seq(32,39) ++
@@ -127,3 +127,93 @@ movimientos() ->
 	lists:seq(62,69) ++
 	lists:seq(72,79) ++
 	lists:seq(82,89).
+
+
+%Valor de cada posición en el tablero, los valores
+%fueron tomados y adaptados del score asignado al juego
+%reversi de Microsoft Windows http://www.samsoft.org.uk/reversi/strategy.htm#position
+
+
+%	  _, C, A, B, B, A, C, _, 
+%     C, X, 0, 0, 0, 0, X, C, 
+%     A, 0, 0, 0, 0, 0, 0, A, 
+%     B, 0, 0, 0, 0, 0, 0, B, 
+%     B, 0, 0, 0, 0, 0, 0, B, 
+%     A, 0, 0, 0, 0, 0, 0, A, 
+%     C, X, 0, 0, 0, 0, X, C, 
+%     _, C, A, B, B, A, C, _, 
+
+
+
+%Esquinas
+score(12) -> 99;
+score(19) -> 99;
+score(82) -> 99;
+score(89) -> 99;
+
+%Cuadros X
+score(23) -> -24;
+score(28) -> -24;
+score(73) -> -24;
+score(78) -> -24;
+
+%Cuadros C
+score(13) -> -8;
+score(18) -> -8;
+score(22) -> -8;
+score(29) -> -8;
+score(72) -> -8;
+score(79) -> -8;
+score(83) -> -8;
+score(88) -> -8;
+
+%Cuadros A
+score(14) -> 8;
+score(17) -> 8;
+score(32) -> 8;
+score(39) -> 8;
+score(62) -> 8;
+score(69) -> 8;
+score(84) -> 8;
+score(87) -> 8;
+
+%Cuadros B
+score(15) -> 6;
+score(16) -> 6;
+score(42) -> 6;
+score(49) -> 6;
+score(52) -> 6;
+score(59) -> 6;
+score(85) -> 6;
+score(86) -> 6;
+
+%Otros
+score(24) -> -4;
+score(27) -> -4;
+score(33) -> -4;
+score(38) -> -4;
+score(63) -> -4;
+score(68) -> -4;
+score(74) -> -4;
+score(77) -> -4;
+
+score(25) -> -3;
+score(26) -> -3;
+score(43) -> -3;
+score(48) -> -3;
+score(53) -> -3;
+score(58) -> -3;
+score(75) -> -3;
+score(76) -> -3;
+
+score(34) -> 7;
+score(37) -> 7;
+score(64) -> 7;
+score(67) -> 7;
+
+score(44) -> 4;
+score(47) -> 4;
+score(54) -> 4;
+score(57) -> 4;
+
+score(_) -> 0.
